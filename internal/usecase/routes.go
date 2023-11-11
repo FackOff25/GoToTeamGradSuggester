62fc245cf055ca81db9bae4a4aa7d79f48504e02
FackOff25 <54987169+FackOff25@users.noreply.github.com>
2023-11-11 13:47:03 +0300

    sort places interface

--- internal/usecase/routes.go
+++ internal/usecase/routes.go
@@ -2,6 +2,10 @@ package usecase
 
 import "github.com/FackOff25/GoToTeamGradSuggester/internal/domain"
 
-func sortPlacesForRoute(places []domain.ApiLocation) []domain.ApiLocation {
+type PlaceToSort interface {
+	GetLocation() domain.ApiLocation
+}
+
+func sortPlacesForRoute(places []PlaceToSort) []PlaceToSort {
 	return places
 }
