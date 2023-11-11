565e685c1fb7b1fb14c1795accb3251e9df61dd8
Александр Ф <al@Apollo.local>
2023-11-11 18:11:17 +0300

    merged chages

--- internal/usecase/route.go
+++ internal/usecase/route.go
@@ -5,36 +5,14 @@ import (
 
 	"github.com/FackOff25/GoToTeamGradSuggester/internal/domain"
 	"github.com/FackOff25/GoToTeamGradSuggester/pkg/gamilton"
+
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/FackOff25/GoToTeamGradGoLibs/googleApi"
 )
-
-func (uc *UseCase) GetRoute(req *domain.RouteReq) (*domain.Route, error) {
-	if req.TravelMode == "" {
-		req.TravelMode = domain.TravelModeWalk
-	}
-
-	unsortedPlaces := make([]domain.ApiLocation, 0)
-	for _, v := range req.Waypoints {
-		unsortedPlaces = append(unsortedPlaces, v.Location)
-	}
-
-	sortedPlaces := sortPlacesForRoute(unsortedPlaces)
-	Greq := domain.GrouteRequest{
-		TravelMode:    req.TravelMode,
-		Intermediates: make([]domain.Gwaypoint, 0),
-	}
-
-	for i, v := range sortedPlaces {
-		if i == 0 {
-			Greq.Origin = domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}}
-		} else if i == len(sortedPlaces)-1 {
-			Greq.Destination = domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}}
-		} else {
-			Greq.Intermediates = append(Greq.Intermediates, domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}})
-		}
-	}
-
-	return nil, nil
-}
 
 // first element is starting point, second is last (can be the same one)
 func sortPlacesForRoute(places []domain.ApiLocation) []domain.ApiLocation {
@@ -73,4 +51,60 @@ func makeGraphMatrix(places []domain.ApiLocation) [][]float64 {
 	}
 
 	return matrix
+}
+
+
+func (uc *UseCase) GetRoute(req *domain.RouteReq) (*domain.Route, error) {
+	if req.TravelMode == "" {
+		req.TravelMode = domain.TravelModeWalk
+	}
+
+	unsortedPlaces := make([]domain.ApiLocation, 0)
+	for _, v := range req.Waypoints {
+		unsortedPlaces = append(unsortedPlaces, v.Location)
+	}
+
+	sortedPlaces := sortPlacesForRoute(unsortedPlaces)
+	GreqBody := domain.GrouteRequest{
+		TravelMode:    req.TravelMode,
+		Intermediates: make([]domain.Gwaypoint, 0),
+	}
+
+	for i, v := range sortedPlaces {
+		if i == 0 {
+			GreqBody.Origin = domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}}
+		} else if i == len(sortedPlaces)-1 {
+			GreqBody.Destination = domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}}
+		} else {
+			GreqBody.Intermediates = append(GreqBody.Intermediates, domain.Gwaypoint{Glocation: domain.Glocation{GlatLng: domain.GlatLng(v)}})
+		}
+	}
+
+	BytesGreqBody, err := json.Marshal(GreqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	request := uc.cfg.RoutesApiHost + "directions/v2:computeRoutes"
+
+	client := &http.Client{}
+	Grequest, err := http.NewRequest(http.MethodPost, request, bytes.NewReader(BytesGreqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	Grequest.Header.Set("Proxy-Header", "go-explore")
+	resp, err := client.Do(Grequest)
+	if err != nil {
+		
+	}
+
+	data, _ := io.ReadAll(resp.Body)
+	var result googleApi.NearbyPlacesAnswer
+	json.Unmarshal(data, &result)
+
+	if result.Status != googleApi.STATUS_OK && result.Status != googleApi.STATUS_ZERO_RESULTS {
+	}
+
+	return nil, nil
 }
