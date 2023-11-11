400a4765b309f6a89f9a0a99cbdf86af5c049940
FackOff25 <54987169+FackOff25@users.noreply.github.com>
2023-11-11 13:15:57 +0300

    sort places for route func

---
+++ internal/usecase/routes.go
@@ -0,0 +1,7 @@
+package usecase
+
+import "github.com/FackOff25/GoToTeamGradSuggester/internal/domain"
+
+func sortPlacesForRoute(places []domain.ApiLocation) []domain.ApiLocation {
+	return places
+}
