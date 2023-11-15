9d0f4786735945ff55de7f6f0a6b591a531a1587
FackOff <kozvladimir@bk.ru>
2023-11-15 12:34:05 +0300

    fix

--- pkg/gamilton/gamilton.go
+++ pkg/gamilton/gamilton.go
@@ -9,7 +9,7 @@ func HungryAlgorythm(matrix [][]float64) (path []int) {
 	path[0] = 0
 	path[len(path)-1] = 1
 	checkingVertex := 0
-	for i := 0; i < len(matrix)-1; i++ {
+	for i := 1; i < len(matrix)-1; i++ {
 		min := float64(NEAR_INFINITE_NUMBER)
 		minIdx := checkingVertex
 		for k, v := range matrix[checkingVertex] {
@@ -20,8 +20,8 @@ func HungryAlgorythm(matrix [][]float64) (path []int) {
 				}
 			}
 		}
-		path[i+1] = minIdx
-		checkingVertex = i
+		path[i] = minIdx
+		checkingVertex = minIdx
 	}
 
 	return
